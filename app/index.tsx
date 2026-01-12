d93084207f664a00d592446c4aee358e40d7ed16
helena-varela <helena.varelakg@gmail.com>
2026-01-12 02:21:58 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+
+interface Pokemon{
+  name: string,
+  url: string,
+}
+
+export default function Index() {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    fetchPokemons();
+  }, [])
+
+  async function fetchPokemons(){
+    try{
+      const response = await fetch(
+        "https://pokeapi.co/api/v2/pokemon/?limit=10"
+      );
+
+      const data = await response.json();
+
+      const detailedPokemon = await Promise.all(
+        data.results.map(async (pokemon: Pokemon) => {
+          const res = await fetch(pokemon.url);
+          const details = await res.json();
+          return{
+            name: pokemon.name,
+            image: details.sprites.front_default,
+          };
+        })
+      )
+
+      setPokemons(detailedPokemon);
+
+    }catch(e){
+      console.log(e);
+    }
+  }
+
+  return (
+    <ScrollView>
+      {pokemons.map((pokemon) => (
+        <View key={pokemon.name}>
+          <Text>
+            {pokemon.name}
+          </Text>
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
